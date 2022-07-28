c9645e999328849c2523bb8b11dea31f38b1fa10
Kenny Adeleke <kingken@Kennys-MBP-2.attlocal.net>
2022-07-28 16:01:14 -0500

    update mongodb

--- scripts/mongo-setup.sh
+++ scripts/mongo-setup.sh
@@ -11,3 +11,33 @@ sudo systemctl start mongod
 sudo systemctl status mongod
 sudo systemctl enable mongod
 
+## Adding Data to / Reading Data from a MongoDB Database 
+cat << EOF > /opt/mongodb.js
+//create the names collection and add documents to it
+db.names.insert({'name' : 'Kenny Adeleke'});
+db.names.insert({'name' : 'Peter Campbell'});
+db.names.insert({'name' : 'Betty Draper'});
+db.names.insert({'name' : 'Joan Harris'});
+
+//set a reference to all documents in the database
+allMadMen = db.names.find();
+
+//iterate the names collection and output each document
+while (allMadMen.hasNext()) {
+   printjson(allMadMen.next());
+}
+EOF
+
+## Create script to collect data
+cat >> EOF > /opt/collect-data.js
+//set a reference to all documents in the database
+allMadMen = db.names.find();
+
+//iterate the names collection and output each document
+while (allMadMen.hasNext()) {
+   printjson(allMadMen.next());
+}
+EOF
+
+mongo /opt/mongodb.js
+
